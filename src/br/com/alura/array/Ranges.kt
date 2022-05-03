3c64cce9397bbd9760fa634fe60fa66ed9da226d
VictorArruda22 <victor.ti.henrique@gmail.com>
2022-05-03 12:03:30 -0300

    Working with downTo, until and forEach

--- src/br/com/alura/array/Ranges.kt
+++ src/br/com/alura/array/Ranges.kt
@@ -10,4 +10,24 @@ fun main(){
     for (numeroPar in numerosPares){
         print("$numeroPar ")
     }
+
+    println()
+
+    val numerosParesReverso = 100 downTo 0 step 2
+    numerosParesReverso.forEach{ println("$it") }
+
+    val intervalo = 1500.0..5000.0
+    val salario = 4000.0
+
+    if (salario in intervalo){
+        println("Está dentro do intervalo")
+    }
+    else{
+        println("Não está dentro do intervalo")
+    }
+
+    val alfabeto = 'a'..'z'
+    val letra = 'k'
+
+    print(letra in alfabeto)
 }
