4765ea98f84df5ec53dc86353c591f6fd2f70dab
VictorArruda22 <victor.ti.henrique@gmail.com>
2022-05-03 11:39:14 -0300

    Initial commit

---
+++ src/br/com/alura/array/Ranges.kt
@@ -0,0 +1,13 @@
+package br.com.alura.array
+
+fun main(){
+    val serie: IntRange = 1.rangeTo(10)
+    for (s in serie){
+
+    }
+
+    val numerosPares = 0..100 step 2
+    for (numeroPar in numerosPares){
+        print("$numeroPar ")
+    }
+}
