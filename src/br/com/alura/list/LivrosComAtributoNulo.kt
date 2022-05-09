b0d473605829f7385c6bd5f46a508a2c7bac9086
VictorArruda22 <victor.ti.henrique@gmail.com>
2022-05-09 14:47:34 -0300

    Add Elvis operator

--- src/br/com/alura/list/LivrosComAtributoNulo.kt
+++ src/br/com/alura/list/LivrosComAtributoNulo.kt
@@ -2,7 +2,7 @@ package br.com.alura.list
 
 fun main() {
     listaDeLivros
-        .groupBy { it.editora}
+        .groupBy { it.editora ?: "Editora desconhecida"}
         .forEach { (editora: String?, livros: List<Livro>) ->
             println("$editora - ${livros.joinToString { it.titulo }}")
         }
